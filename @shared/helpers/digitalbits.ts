3b2105afa6af60ff4c30f494f49d930f84ce24f1
Owan Hunte <owanh@hotmail.com>
2021-08-16 23:39:43 -0400

    refactor: swap out `stellar-sdk` for `xdb-digitalbits-sdk` in shared/helpers workspace files

--- @shared/helpers/digitalbits.ts
+++ @shared/helpers/digitalbits.ts
@@ -17,7 +17,7 @@ export const MAINNET_NETWORK_DETAILS = {
   network: NETWORKS.PUBLIC,
   networkName: NETWORK_NAMES.PUBNET,
   otherNetworkName: NETWORK_NAMES.TESTNET,
-  networkUrl: "https://horizon.stellar.org",
+  networkUrl: "https://frontier.livenet.digitalbits.io",
   networkPassphrase: DigitalBitsSdk.Networks.PUBLIC,
 } as NetworkDetails;
 
@@ -26,7 +26,7 @@ export const TESTNET_NETWORK_DETAILS = {
   network: NETWORKS.TESTNET,
   networkName: NETWORK_NAMES.TESTNET,
   otherNetworkName: NETWORK_NAMES.PUBNET,
-  networkUrl: "https://horizon-testnet.stellar.org",
+  networkUrl: "https://frontier.testnet.digitalbits.io",
   networkPassphrase: DigitalBitsSdk.Networks.TESTNET,
 } as NetworkDetails;
 
